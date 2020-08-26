e1678fd1e7ac113f4ed3e2e24f2802405aadf702
Lucas Souza <lucas.souza@redfox.tech>
2020-08-25 21:40:55 -0300

    test: ensure SignUpController return 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -32,3 +32,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamsError('email'))
   })
 })
+
+describe('Signup Controller', () => {
+  test('Shold return 400 if no password is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_emai@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamsError('password'))
+  })
+})
