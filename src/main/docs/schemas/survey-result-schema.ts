bc23dae6da66bf2a4f39af295f575460f76ce8cf
Lucas Souza <lucas.souza@redfox.tech>
2020-11-06 16:29:18 -0300

    docs: add save survey result api docs

---
+++ src/main/docs/schemas/survey-result-schema.ts
@@ -0,0 +1,20 @@
+export const surveyResultSchema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string'
+    },
+    accountId: {
+      type: 'string'
+    },
+    SurveyId: {
+      type: 'string'
+    },
+    answer: {
+      type: 'string'
+    },
+    date: {
+      type: 'string'
+    }
+  }
+}
